66b319601726ad2b7f36e4745eb21ec1e2a3d64e
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2019-02-12 13:48:17 +0100

    macros directory removed

--- sources/macros/rootlogon.C
+++
@@ -1,7 +0,0 @@
-{
-  if(!gSystem->Load("libDesktopDigitizer6.so"))
-    cout<<"libDesktopDigitizer6.so loaded..."<<endl;
-  else
-    cout<<"Problems with loading libDesktopDigitizer6.so"<<endl;
-  gROOT->LoadMacro("SignalsViewer.C");
-}
