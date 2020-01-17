88421dc22ded0a058aa71f1857efc99142ffeb4d
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2020-01-17 17:30:11 +0100

    documentation updated

--- search/all_5.js
+++ search/all_5.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['getamplitude',['GetAmplitude',['../classDDSignal.html#ab728789eab82f8d27a081c576288e6e4',1,'DDSignal']]],
-  ['getcalibrated',['GetCalibrated',['../classDDSignal.html#a133a545a63e2e4b24fe967ef86fef4d2',1,'DDSignal']]],
   ['getcharge',['GetCharge',['../classDDSignal.html#ae710b8b8636b91f0143203b047c88ecc',1,'DDSignal']]],
+  ['getpe',['GetPE',['../classDDSignal.html#a815ebc6591c16324b9e2b21c5fbbe6bf',1,'DDSignal']]],
   ['gett0',['GetT0',['../classDDSignal.html#a32c4364fb803c35f897d8f63cbc49231',1,'DDSignal']]],
   ['gettot',['GetTOT',['../classDDSignal.html#a7b9e6db7a6cc00ed09aa852bb75d8c02',1,'DDSignal']]],
   ['gettree',['GetTree',['../classDDCalib.html#aa0eb53c4c51111fc574776bf4fddccfa',1,'DDCalib']]],
--- search/functions_8.js
+++ search/functions_8.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['setall',['SetAll',['../classDDSignal.html#a44c396308da0bf49e4afb076baa1182e',1,'DDSignal']]],
   ['setamplitude',['SetAmplitude',['../classDDSignal.html#a473243df15ffc45ba7c8ce1c67cf61bf',1,'DDSignal']]],
-  ['setcalibrated',['SetCalibrated',['../classDDSignal.html#a236ef6c8ab2c774b9aff115aa8e32e57',1,'DDSignal']]],
   ['setcharge',['SetCharge',['../classDDSignal.html#ae32096be250fcb72a8fb39c27fd067b3',1,'DDSignal']]],
   ['setfitrange',['SetFitRange',['../classDDCalibPE.html#aff2390bf292348103007a339bb5723f3',1,'DDCalibPE']]],
+  ['setpe',['SetPE',['../classDDSignal.html#ab9bfcb25863e64a31411aff03faea33f',1,'DDSignal']]],
   ['sett0',['SetT0',['../classDDSignal.html#a54dc4ca604d1bcc2a72590d928fd9cf6',1,'DDSignal']]],
   ['settot',['SetTOT',['../classDDSignal.html#adcce658aa1d9cae35ef3210be55625bc',1,'DDSignal']]]
 ];
