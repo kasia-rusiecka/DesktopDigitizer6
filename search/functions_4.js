8514828d2ec3f920cd7423d85d8ba6d1859e6fa2
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2020-01-17 17:32:09 +0100

    documentation updated

--- search/functions_4.js
+++ search/functions_4.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['getamplitude',['GetAmplitude',['../classDDSignal.html#ab728789eab82f8d27a081c576288e6e4',1,'DDSignal']]],
   ['getcharge',['GetCharge',['../classDDSignal.html#ae710b8b8636b91f0143203b047c88ecc',1,'DDSignal']]],
+  ['getnentries',['GetNentries',['../classDDTreeMaker.html#aeaf86cbaa859925aaf574c4e6b3edc30',1,'DDTreeMaker']]],
   ['getpe',['GetPE',['../classDDSignal.html#a815ebc6591c16324b9e2b21c5fbbe6bf',1,'DDSignal']]],
   ['gett0',['GetT0',['../classDDSignal.html#a32c4364fb803c35f897d8f63cbc49231',1,'DDSignal']]],
   ['gettot',['GetTOT',['../classDDSignal.html#a7b9e6db7a6cc00ed09aa852bb75d8c02',1,'DDSignal']]],
